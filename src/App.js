ec4da51edb1bb0d751a6d43781fc93eaf3050cf4
bShinzato <bshinzato@outlook.com>
2023-06-16 00:47:43 -0700

    Does it work?

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Brandon Shinzato</h1>
+        <p>About Me</p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.

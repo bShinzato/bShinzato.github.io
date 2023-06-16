8b72b8c4cbd23642ec4f1804ecd826aa2ef9a356
bShinzato <bshinzato@outlook.com>
2023-06-16 01:16:07 -0700

    update build

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Brandon Shinzato</h1>
-        <h2>About Me</h2>
+        <h2>About Me!</h2>
         <p>I like to do a lot of things! Maybe we have Similarities?!</p>
         <ul>
             <li>Fishing</li>
